f9ac502d8e6041f50dabfcf842fd7877877bc59e
Eric van Zanten <eric.vanzanten@gmail.com>
2016-01-20 12:56:29 -0600

    Update cron task

--- cron_tasks.sh
+++ cron_tasks.sh
@@ -3,4 +3,4 @@
 cd /home/datamade/chi-councilmatic 
 /home/datamade/.virtualenvs/chi-councilmatic/bin/python manage.py loaddata >> /tmp/chicago-loaddata.log 2>&1 
 /home/datamade/.virtualenvs/chi-councilmatic/bin/python manage.py fixdata >> /tmp/chicago-loaddata.log 2>&1 
-/home/datamade/.virtualenvs/chi-councilmatic/bin/python manage.py update_index
+/home/datamade/.virtualenvs/chi-councilmatic/bin/python manage.py update_index --age=24
