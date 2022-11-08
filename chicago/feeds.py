0f810a55372b2268c0f8ba76ea07a4620c7f180d
Derek Eder <derek.eder@gmail.com>
2022-11-08 13:50:24 -0600

    formatting chicago python files

--- chicago/feeds.py
+++ chicago/feeds.py
@@ -1,12 +1,14 @@
 from councilmatic_core.feeds import CouncilmaticFacetedSearchFeed, BillDetailActionFeed
 from chicago.models import ChicagoBill
 
+
 class ChicagoCouncilmaticFacetedSearchFeed(CouncilmaticFacetedSearchFeed):
-    # same as CouncilmaticFacetedSearchFeed but have a better item name template which uses
-    # NYCBill's friendly_name() as opposed to Bill's friendly_name()
-    title_template = 'feeds/chicago_search_item_title.html'
+    # same as CouncilmaticFacetedSearchFeed but have a better item name
+    # template which uses NYCBill's friendly_name() as opposed to Bill's
+    # friendly_name()
+    title_template = "feeds/chicago_search_item_title.html"
     bill_model = ChicagoBill
-    
+
+
 class ChicagoBillDetailActionFeed(BillDetailActionFeed):
-    title_template = 'feeds/chicago_bill_actions_item_title.html'
-    
+    title_template = "feeds/chicago_bill_actions_item_title.html"
