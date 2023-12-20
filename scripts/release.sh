9d5c63b2a507318d7355a2e5fc111a1b2bf47cfb
Derek Eder <derek.eder@gmail.com>
2023-12-20 10:44:15 -0600

    logic for NO_DATABASE env var

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
-if [ -v "${NO_DATABASE}" ]; then
+if [ -z ${NO_DATABASE} ]; then
    echo "NO_DATABASE is set, skipping database setup"
    exit 1
 fi
-
-python manage.py migrate --noinput
-python manage.py import_shapes data/final/chicago_shapes.geojson
-python manage.py populate_person_statistics
-python manage.py collectstatic --noinput
-python manage.py createcachetable
-python manage.py clear_cache
