01efb8544d33dacff183c90038716a87924ac51c
Forest Gregg <fgregg@datamade.us>
2023-12-21 21:07:36 -0500

    try it the other way

--- chicago/settings.py
+++ chicago/settings.py
@@ -37,7 +37,6 @@ if os.getenv("SENTRY_DSN"):
     sentry_sdk.init(
         dsn=os.environ["SENTRY_DSN"],
         integrations=[DjangoIntegration()],
-        release=f"{os.environ['HEROKU_RELEASE_VERSION']}-{os.environ['HEROKU_APP_NAME']}",
     )
 
 DATABASES = {}
