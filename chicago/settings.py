3134459b317411a65f75b3f68a08364b26cafc6e
Forest Gregg <fgregg@datamade.us>
2024-01-09 11:22:28 -0500

    typo in traces sample rate arg

--- chicago/settings.py
+++ chicago/settings.py
@@ -32,7 +32,7 @@ if os.getenv("SENTRY_DSN"):
         integrations=[DjangoIntegration()],
         release=f"{os.environ['HEROKU_RELEASE_VERSION']}-{os.environ['HEROKU_APP_NAME']}",
         enable_tracing=True,
-        traces_sampling_rate=0.05,
+        traces_sample_rate=0.05,
     )
 
 DATABASES = {}
